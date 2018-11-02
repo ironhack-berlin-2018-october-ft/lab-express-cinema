c599d725be0e658735fc0c11b99cb42ac2b98646
Maxence Bouret <maxence.bouret@gmail.com>
2018-11-02 13:12:52 +0100

    Iteration 3 done

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const router  = express.Router();
+const Movie = require("../models/Movie")
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+/* GET movies page */
+router.get('/movies', (req, res, next) => {
+  Movie.find().then(moviesFromDb=> {
+    console.log(movies)
+    res.render('movies', {movies:moviesFromDb});
+  })
+});
+
+
+
 
 module.exports = router;
